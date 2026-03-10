dc98f1b5266e33346a80221fbb52a433717cb575
Gladis <gladyskankwala03@gmail.com>
2026-03-10 21:45:18 +0200

    whatioffer update 1

--- src/components/Whatioffer.jsx
+++ src/components/Whatioffer.jsx
@@ -1,10 +1,27 @@
 
 
+const offers = [
+    {
+       name: "Development",
+       id: 1,
+       para: "Bring designs to life with clean, efficient, secure and high-performing code.",
+       tools: "HTML, CSS, JS & React ",
+       comp: "Cross-Browser Compatibility & Faster Load Times"
+    },
+]
+
 function Whatioffer() {
 
     return(
         <section className="bg-white flex min-h-screen justify-center">
             <h1>What I offer</h1>
+            <div>
+                {offers.map((offer) => (
+                    <div key={offer.id}>
+                        <h2>{}</h2>
+                    </div>
+                ))}
+            </div>
         </section>
     )
 
